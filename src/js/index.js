f43db809fcb674f91d4f2f93fbe5b200ae1749f0
Eric Vignola <ericvignola@Erics-MacBook-Pro-2.local>
2018-02-01 12:39:27 -0500

    Added webpack to handle project dependencies

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+//js dependencies
+import * as d3 from 'd3';
+
+//css dependencies
+import '../css/index.css';
+
+
+const width = 360,
+    height = 360,
+    radius = Math.min(width, height) / 2,
+    innerRadius = Math.min(width, height) / 4;
